77418a6e7115cf2ded754963889aa139391d7e12
Jais Kurecko Eriksen <eaajaeri6@students.eaaa.dk>
2019-05-15 21:10:02 +0200

    style

--- js/script.js
+++ js/script.js
@@ -3,48 +3,50 @@
 let clickedBtn;
 let categoryId;
 let headline;
+
 /*
 document.querySelector('.small-btn').addEventListener('click', function() {
      	clickedBtn = (this.id);
       console.log(clickedBtn);
     });
 */
+
+$(document).ready(function(){
 $('.large-btn').click(function() {
   clickedBtn = (this.id);
   console.log(clickedBtn);
   whatMessage()
   getMessages();
-  $('.msg-modal').css('display','block') ;
+  $('.msg-modal').show() ;
 });
 $('.small-btn').click(function() {
   clickedBtn = (this.id);
   console.log(clickedBtn);
   whatMessage()
   getMessages();
-  $('.msg-modal').css('display','block') ;
+  $('.msg-modal').show() ;
 });
-
-/*
 $('#close').click(function(){
-  $('.msg-modal').css('display','none') ;
+  $('.msg-modal').hide() ;
   console.log('close');
 });
-*/
 
+});
+/*
 document.querySelector('#close').onclick = function() {
   document.querySelector('.msg-modal').style.display = 'none';
   console.log('close');
 };
-
+*/
 
 function whatMessage() {
   if (clickedBtn == "latest-btn") {
     categoryId = 'http://komediehuset.com/wp-json/wp/v2/posts?_embed&categories=46';
-    headline = '<h2>Seneste beskeder<br></2>'
+    headline = '<h2 class="msg-headline">Seneste beskeder<br><br></2>'
   } else
   if (clickedBtn == "hold-a") {
     categoryId = 'http://komediehuset.com/wp-json/wp/v2/posts?_embed&categories=47';
-    headline = '<h2>Beskeder hold A<br></2>'
+    headline = '<h2 class="msg-headline">Beskeder hold A<br></2>'
   }
 };
 
@@ -63,9 +65,10 @@ function appendMessages(messages) {
   for (let message of messages) {
     messageTemplate += `
         <section class="messages">
+        <button id="close">X</button>
           <div class="message">
-            <h4>${message.title.rendered}</h4>
-            <p>Besked: ${message.content.rendered}</p>
+            <h4 class="msg-title">${message.title.rendered}</h4>
+            <p class="msg-content">${message.content.rendered}</p>
           </div>
           </section>
         `;
