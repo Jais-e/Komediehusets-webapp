94e1571a797c7cb443cb0912b4fd583cb145c3f6
Jais Kurecko Eriksen <eaajaeri6@students.eaaa.dk>
2019-05-24 10:28:41 +0200

    checkboxe med mere

--- js/script.js
+++ js/script.js
@@ -7,6 +7,32 @@ let categoryId;
 let headline;
 let pageId;
 
+// Show spcific message when redirected from notification click
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  let regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  let results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+let postSlug = getUrlParameter('postSlug');
+console.log(postSlug);
+if (postSlug != "") {
+  showPost(postSlug);
+}
+function showPost(postSlug) {
+  let url = 'https://komediehuset.com/wp-json/wp/v2/posts?_embed&slug=' + postSlug;
+  console.log(url);
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(messages) {
+      appendMessages(messages);
+      document.querySelector('#msg-modal').style.display = "block";
+    });
+};
+
+
 // Categories
 function getCategories() {
   fetch('https://komediehuset.com/wp-json/wp/v2/categories?parent=46&per_page=100')
@@ -18,41 +44,32 @@ function getCategories() {
       appendCategories(categories);
     });
 };
-
 getCategories();
 
+
+// Make buttons and checkboxes
 function appendCategories(categories) {
   let i = 0;
   for (let category of categories) {
-        if (i < 8){
-          pageId = 1;
-          document.querySelector("#teams-p1").innerHTML += `
+if (category.slug != "faelles-beskeder"){
+    document.querySelector("#teams-p1").innerHTML += `
               <button class="small-btn" id="${category.id}" onclick="showCategory(${category.id})">${category.name}</button>
               `;
-          document.querySelector(".arrow-nav").innerHTML = '<button class="arrow-btn" id="next-btn" onclick="nextPage()">></button>';
-
-        }
-        else if (i >= 8 && i < 16){
-          pageId = 2;
-            document.querySelector("#teams-p2").innerHTML += `
-                <button class="small-btn" id="${category.id}" onclick="showCategory(${category.id})">${category.name}</button>
-                `;
-            document.querySelector(".arrow-nav").innerHTML = '<button class="arrow-btn" id="back-btn"><</button><button class="arrow-btn" id="next-btn" onclick="nextPage()">></button>'
-          }
-        else if (i >= 16 && i < 24) {
-          pageId = 3;
-            document.querySelector("#teams-p3").innerHTML += `
-                <button class="small-btn" id="${category.id}" onclick="showCategory(${category.id})">${category.name}</button>
-                `;
-            document.querySelector(".arrow-nav").innerHTML = '<button class="arrow-btn" id="back-btn"><</button><button class="arrow-btn" id="next-btn" onclick="nextPage()">></button>'
-        }
-
-      };
-i ++;
-
+    document.querySelector("#settings-modal").innerHTML += `
+          <li class="checklist"> <label class="set-label" for="${category.id}">${category.name}</label>
+          <input type="checkbox" class="set-checkbox" id="${category.id}" name="${category.name}"></li>
+                  `;
+  };
 };
 
-function showCategory(id){
+document.querySelector("#settings-modal").innerHTML += `
+<section class="confirm-buttons"><button class="ok-btn" id="save-settings">GEM</button></section>
+`;
+  i++;
+};
+
+// Display message
+function showCategory(id) {
   let url = 'https://komediehuset.com/wp-json/wp/v2/posts?_embed&categories=' + id;
   console.log(url);
   fetch(url)
@@ -61,14 +78,15 @@ function showCategory(id){
     })
     .then(function(messages) {
       appendMessages(messages);
-      $('.msg-modal').show();
+
+      document.querySelector('#msg-modal').style.display = "block";
+
     });
 };
-
-  function appendMessages(messages) {
-    let messageTemplate = "";
-    for (let message of messages) {
-      messageTemplate += `
+function appendMessages(messages) {
+  let messageTemplate = "";
+  for (let message of messages) {
+    messageTemplate += `
         <section class="messages">
         <button id="close" onclick="closeModal()">X</button>
           <div class="message">
@@ -77,27 +95,28 @@ function showCategory(id){
           </div>
           </section>
         `;
-    };
-
-    document.querySelector(".msg-modal").innerHTML = messageTemplate;
   };
 
-  function closeModal() {
-    console.log('close');
-    document.querySelector('.msg-modal').style.display = 'none';
-  };
+  document.querySelector("#msg-modal").innerHTML = messageTemplate;
+};
 
-function nextPage(){
-  if (pageId === 1){
-    document.querySelector('#teams-p1').style.display = 'none';
-    document.querySelector('#teams-p2').style.display = 'flex';
-    console.log('page 2');
-  }else{
-    if (pageId === 2){
-      document.querySelector('#teams-p1').style.display = 'none';
-      document.querySelector('#teams-p2').style.display = 'flex';
-      document.querySelector('#teams-p3').style.display = 'none';
-      document.querySelector('#next-btn').style.display = 'none';
-    }
-  }
+console.log("chosen");
+};
+// Display menu
+function openMenu() {
+  document.querySelector('#menu-modal').style.display = "block";
+};
+
+//display settings
+function openSettings(){
+  document.querySelector('#settings-modal').style.display = "flex";
+};
+
+// Close modals
+function closeModal() {
+  console.log('close');
+  document.querySelector('.modal').style.display = 'none';
+  document.querySelector('#msg-modal').style.display = 'none';
+  document.querySelector('#menu-modal').style.display = 'none';
+  document.querySelector('#settings-modal').style.display = 'none';
 };
